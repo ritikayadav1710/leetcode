269e37dac952b15c5437dcfb09b3c0e62bfb5ba2
Ritika <17.ritikayadav@gmail.com>
2025-01-16 22:47:36 +0530

    Third-maximum-number

---
+++ array-problems/Third-maximum-number.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums); 
+        int count = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] != nums[i + 1]) { 
+                count++;
+            }
+            if (count == 3) {
+                return nums[i]; 
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
