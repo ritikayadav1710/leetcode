9f55cf1d63f7a099bbb0b3ff179731082ef9942c
Ritika <17.ritikayadav@gmail.com>
2025-01-26 20:29:31 +0530

    Group-Anagrams

---
+++ array-problems/Group-Anagrams.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        boolean[] visited = new boolean[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            visited[i] = true;
+            for (int j = i + 1; j < strs.length; j++) {
+                if (isAnagram(strs[i], strs[j])) {
+                    group.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+            result.add(group);
+        }
+        return result;
+    }
+
+    private static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] chars1 = s1.toCharArray();
+        char[] chars2 = s2.toCharArray();
+
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+
+        return Arrays.equals(chars1, chars2);
+    }
+}
